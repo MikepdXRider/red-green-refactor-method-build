5173c2811bc6b71f088a4a2bb983b0b4181263a1
Michael Rider <mikepdxrider@gmail.com>
2021-11-05 10:17:34 -0700

    adds app infrastrcture/skeleton

---
+++ __test__/get-method.test.js
@@ -0,0 +1,21 @@
+
+
+describe('tests getMethod', () => {
+  it('returns GET method', () => {
+    // define a new get http request string
+
+    // declare a variable to hold a getMethod call passing in newHttpRequest
+
+    // expect getMethodResult to equal 'GET'
+  });
+
+  it('returns DELETE method', () => {
+    // define a new delete http request string
+
+    // declare a variable to hold a getMethod call passing in newHttpRequest
+
+    // expect getMethodResult to equal 'DELETE'
+  });
+
+
+});
