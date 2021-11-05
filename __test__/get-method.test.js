69f2685a9060dca78504bd243fbde86b790b36dc
Michael Rider <mikepdxrider@gmail.com>
2021-11-05 10:42:48 -0700

    adds method validation test for funsies

--- __test__/get-method.test.js
+++ __test__/get-method.test.js
@@ -27,5 +27,18 @@ Accept-Language: us-en`;
     expect(method).toEqual('DELETE');
   });
 
+  it('returns invalid method message', () => {
+    // define a new delete http request string
+    const httpRequest =  `PUST / HTTP/1.1
+Host: example.com
+Accept-Language: us-en`;
+
+    // declare a variable to hold a getMethod call passing in newHttpRequest
+    const method = getMethod(httpRequest);
+
+    // expect getMethodResult to equal 'DELETE'
+    expect(method).toEqual('invalid method');
+  });
+
 
 });
