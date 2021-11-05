ffe3d8167ed5a14b2d37b7f03e3f16f23dca3aa0
Michael Rider <mikepdxrider@gmail.com>
2021-11-05 10:28:22 -0700

    adds passing getMethod tests

--- __test__/get-method.test.js
+++ __test__/get-method.test.js
@@ -3,8 +3,7 @@ const getMethod = require('../lib/get-method.js');
 describe('tests getMethod', () => {
   it('returns GET method', () => {
     // define a new get http request string
-    const httpRequest =  `
-GET / HTTP/1.1
+    const httpRequest =  `GET / HTTP/1.1
 Host: example.com
 Accept-Language: us-en`;
 
@@ -17,10 +16,9 @@ Accept-Language: us-en`;
 
   it('returns DELETE method', () => {
     // define a new delete http request string
-    const httpRequest =  `
-    DELETE / HTTP/1.1
-    Host: example.com
-    Accept-Language: us-en`;
+    const httpRequest =  `DELETE / HTTP/1.1
+Host: example.com
+Accept-Language: us-en`;
 
     // declare a variable to hold a getMethod call passing in newHttpRequest
     const method = getMethod(httpRequest);
