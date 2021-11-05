4a49b62eee52e7105ad56d06fcc8eac6e90472b9
Michael Rider <mikepdxrider@gmail.com>
2021-11-05 10:18:11 -0700

    adds failing getMethod tests

--- __test__/get-method.test.js
+++ __test__/get-method.test.js
@@ -1,20 +1,32 @@
-
+const getMethod = require('../lib/get-method.js');
 
 describe('tests getMethod', () => {
   it('returns GET method', () => {
     // define a new get http request string
+    const httpRequest =  `
+GET / HTTP/1.1
+Host: example.com
+Accept-Language: us-en`;
 
     // declare a variable to hold a getMethod call passing in newHttpRequest
+    const method = getMethod(httpRequest);
 
-    // expect getMethodResult to equal 'GET'
+    // expect method to equal 'GET'
+    expect(method).toEqual('GET');
   });
 
   it('returns DELETE method', () => {
     // define a new delete http request string
+    const httpRequest =  `
+    DELETE / HTTP/1.1
+    Host: example.com
+    Accept-Language: us-en`;
 
     // declare a variable to hold a getMethod call passing in newHttpRequest
+    const method = getMethod(httpRequest);
 
     // expect getMethodResult to equal 'DELETE'
+    expect(method).toEqual('DELETE');
   });
 
 
